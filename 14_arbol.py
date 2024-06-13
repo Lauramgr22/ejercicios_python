46f64f226ef0ee50f7bff6d5260e35d12a9b4682
Lauramgr22 <lauramgr6@gmail.com>
2024-06-13 14:52:57 -0500

    Feat: Se agrega archivo arbol.py

---
+++ 14_arbol.py
@@ -0,0 +1,28 @@
+def arbol(numero):
+    if numero%2 != 0 or numero%2 ==0:
+        base = '*' * numero
+        base1= '-' *numero
+        medio= int(numero/2)-1
+        espacio = int((numero-1)/2)
+        for impar in range(1, numero-4, 2):
+            imparstr = '*'*impar
+            print('\033[1;32m'+' ' * espacio +imparstr)
+            espacio = espacio - 1
+        espacio = int((numero-1)/2)
+        for impar in range(1, numero-2, 2):
+            imparstr = '*'*impar
+            print('\033[1;32m'+' ' * espacio +imparstr)
+            espacio = espacio - 1
+        espacio = int((numero-1)/2)
+        for impar in range(1, numero, 2):
+            imparstr = '*'*impar
+            print('\033[1;32m'+' ' * espacio +imparstr)
+            espacio = espacio - 1
+        print('\033[1;32m'+base)
+        print('\033[1;37m'+base1)
+        print('\033[1;37m'+' '*medio+'* *')
+        print('\033[1;37m'+' '*medio+'* *')
+
+    return
+
+arbol(11)
